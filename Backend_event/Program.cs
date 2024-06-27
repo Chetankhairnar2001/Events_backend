d04f8c5ac7f3aa21e024cd9f39fe1abb5dd6cc68
Chetankhairnar2001 <chetan.khairnar2001@gmail.com>
2024-06-27 13:16:55 -0500

    added cors

--- Backend_event/Program.cs
+++ Backend_event/Program.cs
@@ -1,5 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-
+//cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            //replace localhost with yours
+            //also add your deployed website
+            policy.WithOrigins("http://localhost:4200", "https://localhost:4200",
+                                "https://MyChatRoom.com").AllowAnyMethod().AllowAnyHeader();
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -21,5 +32,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+//Cors
+app.UseCors();
+//make sure the cors line is above this
 app.Run();
